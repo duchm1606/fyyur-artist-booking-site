721b162b3d78fbecaebd5987af0e97332069deff
itsnot-aduck <hoangduc1662002@gmail.com>
2024-04-01 15:04:12 +0700

    mod: add configuration database

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import platform
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,5 +10,14 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+## TODO IMPLEMENT DATABASE URL
+if platform.system() == 'Windows':
+    # For Windows Environment
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/fyyur'
+else:
+    # For linux Environment
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
+
+
+##  Track Modifications
+SQLALCHEMY_TRACK_MODIFICATIONS = False
